e4682cf9865a072d765298a7e3b307944125503b
shruti-vadlamani <saishrutivadlamani@gmail.com>
2025-06-17 15:10:21 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,9 @@
+public class Arithmetic {
+	public static void main(String[] args) {
+		int n1=10, n2=5;
+		System.out.println("addition is : "+(n1+n2));
+		System.out.println("subtraction is : "+(n1-n2));
+		System.out.println("multiplication is : "+(n1*n2));
+		System.out.println("division is : "+(n1/n2));
+	}
+}
